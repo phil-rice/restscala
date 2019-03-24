473348fe1568d80903adaedb15a8dc99c5bd759a
Phil <phil.rice@iee.org>
2019-03-24 23:36:09 +0100

    WIP: Migrating towards JAvascript defined by LensDsl

--- demo/backend3/src/test/scala/one/xingyi/scriptBackend3/JavascriptMadeFromProjectsSpec.scala
+++ demo/backend3/src/test/scala/one/xingyi/scriptBackend3/JavascriptMadeFromProjectsSpec.scala
@@ -17,14 +17,14 @@ class JavascriptMadeFromProjectsSpec extends UtilsSpec {
     //    val lensDefns: immutable.Seq[LensDefnFromProjection[_, _]] = projectionToLensDefns(Person.projection).map(_._2)
     val lensLines = lensDefns.map(_.lensLine)
     lensLines.map(implicitly[LensLineToJavascript]) shouldBe List(
-      """function lens_address_line1_string(){return lens("line1")};""",
-      """function lens_address_line2_string(){return lens("line2")};""",
-      """function lens_person_address_address(){return compose(lens("addresses"),lensForItemInList(0))};""",
-      """function lens_person_addresses_addresslist(){return lens("addresses")};""",
-      """function lens_person_line1_string(){return compose(lens("addresses"),lensForItemInList(0),lens("line1"))};""",
-      """function lens_person_line2_string(){return compose(lens("addresses"),lensForLastItemInList,lens("line2"))};""",
-      """function lens_person_name_string(){return lens("name")};""",
-      """function lens_person_telephonenumber_telephonenumber(){return lens("telephoneNumber")};""",
-      """function lens_telephonenumber_number_string(){return lens("number")};""")
+        """function lens_address_line1_string(){return lens("line1")};""",
+        """function lens_address_line2_string(){return lens("line2")};""",
+        """function lens_person_address_address(){return compose(lens("addresses"),lensForItemInList(0))};""",
+        """function lens_person_addresses_addresslist(){return lens("addresses")};""",
+        """function lens_person_line1_string(){return compose(lens("addresses"),compose(lensForItemInList(0),lens("line1")))};""",
+        """function lens_person_line2_string(){return compose(lens("addresses"),compose(lensForLastItemInList(),lens("line2")))};""",
+        """function lens_person_name_string(){return lens("name")};""",
+        """function lens_person_telephonenumber_telephonenumber(){return lens("telephoneNumber")};""",
+        """function lens_telephonenumber_number_string(){return lens("number")};""")
   }
 }
