9d9f0f401fe6cf75a90bd85acb9c0bcead6f1465
Phil <phil.rice@iee.org>
2019-03-24 23:31:17 +0100

    WIP: Migrating towards JAvascript defined by LensDsl

--- modules/javascriptserver/src/main/scala/one/xingyi/javascript/server/LensLineToJavascript.scala
+++ modules/javascriptserver/src/main/scala/one/xingyi/javascript/server/LensLineToJavascript.scala
@@ -2,25 +2,31 @@ package one.xingyi.javascript.server
 import one.xingyi.core.optics._
 import one.xingyi.core.strings.Strings
 
+import scala.collection.GenTraversableOnce
+
 trait LensLineToJavascript extends (LensLine => String)
 
 object LensLineToJavascript extends LensLineToJavascript {
   implicit val lensLineToJavascript: LensLineToJavascript = LensLineToJavascript
+  def one: Function[LensDefn[_, _], Traversable[String]] = {
+    case s: StringLensDefn[_] => None //Some("stringLens()")
+    case d: DoubleLensDefn[_] => None //Some("doubleLens()")
+    case i: IntegerLensDefn[_] => None //Some("integerLens()")
+    case b: BooleanLensDefn[_] => None //Some("booleanLens()")
+    //      case l: ItemInListDefn[_] => None
+    case v: ViewLensDefn[_, _] => None
+    case l: ListLensDefn[_, _] => None
+    case l: ItemInListDefn[_] => Some("lensForItemInList" + Strings.bracket(l.n.toString))
+    case l: LastItemInListDefn[_] => Some("lensForLastItemInList()")
+    case c: ChildLensDefn[_] => Some("lens" + Strings.bracket(Strings.quote(c.name)))
+  }
+  def recurse(defns: List[String]): String = defns match {
+    case defn :: Nil => defn
+    case head :: tail => "compose(" + head + "," + recurse(tail) +")"
+  }
   override def apply(lensLine: LensLine): String = {
-    val body = lensLine.defns.flatMap {
-      case s: StringLensDefn[_] => None //Some("stringLens()")
-      case d: DoubleLensDefn[_] => None //Some("doubleLens()")
-      case i: IntegerLensDefn[_] => None //Some("integerLens()")
-      case b: BooleanLensDefn[_] => None //Some("booleanLens()")
-      //      case l: ItemInListDefn[_] => None
-      case v: ViewLensDefn[_, _] => None
-      case l: ListLensDefn[_, _] => None
-      case l: ItemInListDefn[_] => Some("lensForItemInList" + Strings.bracket(l.n.toString))
-      case l: LastItemInListDefn[_] => Some("lensForLastItemInList")
-      case c: ChildLensDefn[_] => Some("lens" + Strings.bracket(Strings.quote(c.name)))
-    }
-    val bodyString = body.mkString(",")
-    val withCompose = if (body.size > 1) "return compose(" + bodyString + ")" else "return " + bodyString
-    s"""function ${lensLine.name}(){$withCompose};"""
+    val bodyStrings = lensLine.defns.flatMap(one)
+    val bodyString = recurse(bodyStrings)
+    s"""function ${lensLine.name}(){return $bodyString};"""
   }
 }
